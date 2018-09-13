74a3d30493db56c8af0721243709d8bf4052f3f3
Yuuichi Fujioka <fujioka.yuuichi@gmail.com>
2018-09-13 18:57:34 +0900

    fix: port number on example.sh

--- tools/example.sh
+++ tools/example.sh
@@ -3,16 +3,16 @@
 
 docker-compose exec etcd etcdctl set /dns/com/example/foo '{"host": "192.168.0.101"}'
 
-dig -p 1053 @localhost foo.example.com +noall +answer
+dig -p 53 @localhost foo.example.com +noall +answer
 
 docker-compose exec etcd etcdctl set /dns/com/example/foo '{"host": "192.168.0.101"}'
 docker-compose exec etcd etcdctl set /dns/com/example/bar '{"host": "foo.example.com"}'
 
-dig -p 1053 @localhost bar.example.com +noall +answer
+dig -p 53 @localhost bar.example.com +noall +answer
 
 docker-compose exec etcd etcdctl set /dns/com/example/baz/1 '{"host": "192.168.0.101"}'
 docker-compose exec etcd etcdctl set /dns/com/example/baz/2 '{"host": "192.168.0.102"}'
 
-dig -p 1053 @localhost baz.example.com +noall +answer
-dig -p 1053 @localhost baz.example.com +noall +answer
-dig -p 1053 @localhost baz.example.com +noall +answer
+dig -p 53 @localhost baz.example.com +noall +answer
+dig -p 53 @localhost baz.example.com +noall +answer
+dig -p 53 @localhost baz.example.com +noall +answer
